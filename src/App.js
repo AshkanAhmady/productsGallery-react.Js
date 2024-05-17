a6be55bf6b2829eb0350d4ada91695323272108d
unknown <user.name>
2024-05-17 23:02:43 +0330

    (index.js) converted to TS file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import HookProductList from "./Components/ProductList/HookProductList";
 import AppStyles from "./App.module.css";
 import NavBar from "./Components/nav/navBar";
